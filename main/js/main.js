4c41ff66f167ffbc3311183e7d9496ec09c5d633
mdss@makexhtml.ru <mdss@makexhtml.ru>
2017-08-16 17:13:07 +0300

    исправление вызова левого меню

--- main/js/main.js
+++ main/js/main.js
@@ -2,38 +2,42 @@
 	"use strict";
 
 	// mobile menu
-	if ($(window).width() < 992) {
-		var menu = $('.menu');
-		$('.burger').click(function(e) {
-			$(menu).addClass('menu--active');
-			$('body').prepend('<div class="menu__cover"></div>');
-			$('html').css('overflow','hidden');
-			e.preventDefault();
-		});
-		$('body').on('click', '.menu__cover', function() {
-			$(menu).removeClass('menu--active');
-			$('.menu__cover').remove();
-			$('html').css('overflow','auto');
-		});
-		$('body').swipe({
-			swipeRight:function() {
+	$(window).on('load resize', function() {
+		if ($(window).width() < 992) {
+			var menu = $('.menu');
+			$('.burger').click(function(e) {
 				$(menu).addClass('menu--active');
-				if (!$('.menu__cover').length) $('body').prepend('<div class="menu__cover"></div>');
+				$('body').prepend('<div class="menu__cover"></div>');
 				$('html').css('overflow','hidden');
-			},
-			threshold:40,
-			excludedElements:$.fn.swipe.defaults.excludedElements+', .press',
-			preventDefaultEvents: false,
-		});
-		$('.menu').swipe({
-			swipeLeft:function() {
+				e.preventDefault();
+			});
+			$('body').on('click', '.menu__cover', function() {
 				$(menu).removeClass('menu--active');
 				$('.menu__cover').remove();
 				$('html').css('overflow','auto');
-			},
-			threshold:40
-		});
-	}
+			});
+			$('body').swipe({
+				swipeRight:function() {
+					$(menu).addClass('menu--active');
+					if (!$('.menu__cover').length) $('body').prepend('<div class="menu__cover"></div>');
+					$('html').css('overflow','hidden');
+				},
+				threshold:40,
+				excludedElements:$.fn.swipe.defaults.excludedElements+', .press',
+				preventDefaultEvents: false,
+			});
+			$('.menu').swipe({
+				swipeLeft:function() {
+					$(menu).removeClass('menu--active');
+					$('.menu__cover').remove();
+					$('html').css('overflow','auto');
+				},
+				threshold:40
+			});
+		} else {
+			$('body').swipe('destroy');
+		}
+	});
 
 	// press talking
 	if ($(window).width() < 992) {
@@ -41,5 +45,7 @@
 			prevNextButtons: false,
 		});
 	}
+
+	console.log('%c Верстка: mdss@makexhtml.ru ', 'color:#fff;font-size:1.2rem;background-color:#3469c6;')
 
 })(jQuery);
