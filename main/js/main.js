2a343bbedf27ed75e8e902bcdf5ee573dd40b38d
mdss@makexhtml.ru <mdss@makexhtml.ru>
2017-08-16 14:12:57 +0300

    десктопная версия главной страницы

--- main/js/main.js
+++ main/js/main.js
@@ -2,40 +2,44 @@
 	"use strict";
 
 	// mobile menu
-	var menu = $('.menu');
-	$('.burger').click(function(e) {
-		$(menu).addClass('menu--active');
-		$('body').prepend('<div class="menu__cover"></div>');
-		$('html').css('overflow','hidden');
-		e.preventDefault();
-	});
-	$('body').on('click', '.menu__cover', function() {
-		$(menu).removeClass('menu--active');
-		$('.menu__cover').remove();
-		$('html').css('overflow','auto');
-	});
-	$('body').swipe({
-		swipeRight:function() {
+	if ($(window).width() < 992) {
+		var menu = $('.menu');
+		$('.burger').click(function(e) {
 			$(menu).addClass('menu--active');
-			if (!$('.menu__cover').length) $('body').prepend('<div class="menu__cover"></div>');
+			$('body').prepend('<div class="menu__cover"></div>');
 			$('html').css('overflow','hidden');
-		},
-		threshold:40,
-		excludedElements: '.press',
-		preventDefaultEvents: false,
-	});
-	$('.menu').swipe({
-		swipeLeft:function() {
+			e.preventDefault();
+		});
+		$('body').on('click', '.menu__cover', function() {
 			$(menu).removeClass('menu--active');
 			$('.menu__cover').remove();
 			$('html').css('overflow','auto');
-		},
-		threshold:40
-	});
+		});
+		$('body').swipe({
+			swipeRight:function() {
+				$(menu).addClass('menu--active');
+				if (!$('.menu__cover').length) $('body').prepend('<div class="menu__cover"></div>');
+				$('html').css('overflow','hidden');
+			},
+			threshold:40,
+			excludedElements:$.fn.swipe.defaults.excludedElements+', .press',
+			preventDefaultEvents: false,
+		});
+		$('.menu').swipe({
+			swipeLeft:function() {
+				$(menu).removeClass('menu--active');
+				$('.menu__cover').remove();
+				$('html').css('overflow','auto');
+			},
+			threshold:40
+		});
+	}
 
 	// press talking
-	$('.js-press').flickity({
-		prevNextButtons: false,
-	});
+	if ($(window).width() < 992) {
+		$('.js-press').flickity({
+			prevNextButtons: false,
+		});
+	}
 
 })(jQuery);
