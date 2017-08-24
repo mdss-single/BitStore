27a44e1f608bcd075db23eb85234b36ed5f43031
mdss@makexhtml.ru <mdss@makexhtml.ru>
2017-08-24 13:58:21 +0300

    Начата работа над админкой. Добавлена страница настроек.

---
+++ admin/js/main.js
@@ -0,0 +1,32 @@
+(function($){ 
+	"use strict";
+
+	$('html').click(function() {
+		$('.settings__modal--active').removeClass('settings__modal--active');
+	});
+
+	$('.tabs__nav').each(function(i) {
+		var storage = localStorage.getItem('tab' + i);
+		if (storage) {
+			$(this).find('div').removeClass('tabs__nav-item--active').eq(storage).addClass('tabs__nav-item--active').closest('.tabs').find('.tab__content').removeClass('tab__content--active').eq(storage).addClass('tab__content--active');
+		}
+	});
+	$('.tabs__nav').on('click', 'div:not(.tabs__nav-item--active)', function() {
+		$(this).addClass('tabs__nav-item--active').siblings().removeClass('tabs__nav-item--active').closest('.tabs').find('.tab__content').removeClass('tab__content--active').eq($(this).index()).addClass('tab__content--active');
+		var ulIndex = $('.tabs__nav').index($(this).parents('.tabs__nav'));
+		localStorage.removeItem('tab' + ulIndex);
+		localStorage.setItem('tab' + ulIndex, $(this).index());
+	});
+
+	$('.js-modal').on('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		$(this).next().toggleClass('settings__modal--active');
+	});
+	$('.settings__modal').click(function(e) {
+		e.stopPropagation();
+	});
+
+	console.log('%c Верстка: mdss@makexhtml.ru ', 'color:#fff;font-size:1.2rem;background-color:#3469c6;')
+
+})(jQuery);
