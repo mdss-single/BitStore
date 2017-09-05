b2a4fefa4388210887ddc7781e27bab38508eead
mdss@makexhtml.ru <mdss@makexhtml.ru>
2017-09-05 22:47:25 +0300

    модальное окно отправить. Шаг 1

--- wallet/desktop/js/main.js
+++ wallet/desktop/js/main.js
@@ -1,6 +1,8 @@
 (function($){ 
 	"use strict";
 
+	$('.select, .radio, .checkbox').styler();
+
 	$('html').click(function() {
 		$('.wallet__menu--active').removeClass('wallet__menu--active');
 		$('.header__notice-link').removeClass('header__notice-link--active');
@@ -43,6 +45,28 @@
 	});
 	$('.js-hello-next').click(function() {
 		helloCarousel.flickity('next');
+	});
+
+	$('.js-send-custom-link').click(function(e) {
+		$('.js-send-auto').removeClass('send__settings--active');
+		$('.js-send-custom').addClass('send__settings--active');
+		e.preventDefault();
+	});
+	$('.js-send-custom-cancel').click(function(e) {
+		$('.js-send-custom').removeClass('send__settings--active');
+		$('.js-send-auto').addClass('send__settings--active');
+		e.preventDefault();
+	});
+
+	$('.js-wallet-send').click(function() {
+		$('body').prepend('<div class="modal__bg"></div>');
+		$('.send').addClass('send--active');
+		$('.send').find('input[autofocus]').focus();
+	});
+	$('body').on('click', '.send__close, .modal__bg', function(e) {
+		$('.modal__bg').remove();
+		$('.send--active').removeClass('send--active');
+		e.preventDefault();
 	});
 
 	console.log('%c Верстка: mdss@makexhtml.ru ', 'color:#fff;font-size:1.2rem;background-color:#3469c6;')
