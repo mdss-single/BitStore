09f363daa0c8ae43d6d91cdbc08c4f71a26f9ca3
mdss@makexhtml.ru <mdss@makexhtml.ru>
2017-09-05 20:41:13 +0300

    hello window

--- wallet/desktop/js/main.js
+++ wallet/desktop/js/main.js
@@ -26,6 +26,25 @@
 		e.stopPropagation();
 	});
 
+	var helloCarousel = $('.js-hello').flickity({
+		cellSelector: '.hello__item',
+		prevNextButtons: false,
+	});
+	var storage = localStorage.getItem('hello');
+	if (!storage) {
+		$('body').prepend('<div class="hello__bg"></div>');
+		$('.hello').addClass('hello--active');
+		helloCarousel.flickity('resize');
+	}
+	$('.js-hello-skip').click(function() {
+		$('.hello__bg').remove();
+		$('.hello').removeClass('hello--active');
+		localStorage.setItem('hello', true);
+	});
+	$('.js-hello-next').click(function() {
+		helloCarousel.flickity('next');
+	});
+
 	console.log('%c Верстка: mdss@makexhtml.ru ', 'color:#fff;font-size:1.2rem;background-color:#3469c6;')
 
 })(jQuery);
